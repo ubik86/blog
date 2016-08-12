411eba6487afa6f1599b5a01b282a2fa3810501b
Boguslaw Tarnowski <boguslaw.tarnowski@blstream.com>
2016-08-12 10:53:06 +0200

    Fix models/post_spec.rb descriptions, Add 2 tests to models/post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,9 +3,17 @@ require 'rails_helper'
 post = FactoryGirl.build(:post)
 
 RSpec.describe Post, type: :model do
-  context 'when logged in' do
-    it 'should present posts for user' do 
+  context 'when has posts' do
+    it 'should post has title' do 
       expect(post.title).to eq 'Post'
+    end
+
+    it 'should post has content' do 
+      expect(post.content).to eq 'Content'
+    end
+
+    it 'should post has published' do 
+      expect(post.published).to eq false
     end
   end
 end
