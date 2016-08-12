16c7596f27b0b2782b71955cfcf1ef663ad902cc
Boguslaw Tarnowski <boguslaw.tarnowski@blstream.com>
2016-08-12 16:11:10 +0200

    Add quantity of comments to Post model, new empty partial _children.haml

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,9 +3,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   has_one :user, through: :post
-  has_ancestry 
-
-  #has_many :children, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
+  has_ancestry
 
 
+  # return quantity of childrens
+  def childrens
+    self.descendants.size
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,15 @@ class Post < ActiveRecord::Base
   def self.search(search)
     where(["title LIKE ?  OR content LIKE ? ", "%#{search}%", "%#{search}%"])
   end
+
+
+  # return quantity of comments and all childrens
+  def c_quantity
+    quantity=0 
+
+    self.comments.each do |c|
+      quantity += 1 + c.descendants.size
+    end
+    quantity
+  end
 end
