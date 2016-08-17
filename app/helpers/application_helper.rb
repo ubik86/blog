373ceff9a5745e90c69bb8c3b6d347f0c493faaf
Boguslaw Tarnowski <boguslaw.tarnowski@blstream.com>
2016-08-17 08:29:25 +0200

    Add helper method active_controller, dynamic class active on application/layout view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+
+  # this helper method checks active controller and return .active used in application/layout
+  def active_controller(name)
+    controller.controller_name==name ? 'active' : nil
+  end
 end
